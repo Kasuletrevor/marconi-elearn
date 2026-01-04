24ec15e0832e41e303b495ea1fce1e0665c396d8
Kasuletrevor <kasulejohntrevor@gmail.com>
2026-01-04 03:43:56 +0300

    chore(models): add session to exports

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.course import Course
 from app.models.organization import Organization
 from app.models.organization_membership import OrganizationMembership
+from app.models.session import Session
 from app.models.user import User
 
-__all__ = ["Course", "Organization", "OrganizationMembership", "User"]
+__all__ = ["Course", "Organization", "OrganizationMembership", "Session", "User"]
