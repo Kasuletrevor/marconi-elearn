0920d693336f9586472f9f9b11610b37d9285a63
Kasuletrevor <kasulejohntrevor@gmail.com>
2026-01-07 21:22:24 +0300

    feat(frontend): add head component with Google Fonts

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@100..1000&family=JetBrains+Mono:wght@100..800&family=Playfair+Display:wght@400..900&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
+}
